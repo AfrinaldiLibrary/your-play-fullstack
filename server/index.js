01595554587e6edaeb5d7d6e364cb5dca4df5673
afrinaldiGenzo <afrinaldi190401@gmail.com>
2023-08-14 23:48:53 +0700

    ADD: FrontEnd

--- server/index.js
+++ server/index.js
@@ -1,31 +1,42 @@
-const express = require('express')
-require('dotenv').config()
-const {
-    videos
-} = require('./data')
+const express = require("express");
+require("dotenv").config();
+const { videos } = require("./data");
 
-const app = express()
-const db = require('./app/models/')
+const app = express();
+const db = require("./app/models/");
 
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-require('./app/routes/videos')(app)
+app.use(cors(corsOptions));
 
-const PORT = process.env.PORT
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+require("./app/routes/videos")(app);
+
+const PORT = process.env.PORT;
 db.mongoose
-    .connect(db.url)
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
-        /* tambah data dummy */
-        // db.videos.insertMany(videos)
-        //     .then(() => {
-        //         console.log('Data inserted successfully.');
-        //     })
-        //     .catch((error) => {
-        //         console.error('Error inserting data:', error);
-        //     })
-    })
-    .catch((error) => console.log(`${error} did not connect`))
+  .connect(db.url)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server is running on http://localhost:${PORT}`)
+    );
+    /* tambah data dummy */
+    // db.videos.insertMany(videos)
+    //     .then(() => {
+    //         console.log('Data inserted successfully.');
+    //     })
+    //     .catch((error) => {
+    //         console.error('Error inserting data:', error);
+    //     })
+  })
+  .catch((error) => console.log(`${error} did not connect`));
